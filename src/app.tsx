50a72b4fb688ba55dfa4b16ec9825edece3275e6
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2025-03-25 15:27:29 -0700

    feat: init

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Counter from "./components/counter";
+
+export function App() {
+  return (
+    <main>
+      Home 123
+      <Counter />
+    </main>
+  );
+}
---
+++ src/components/counter/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as styles from "./counter.modules.css";
+
+export default function Counter() {
+  return <div className={styles.counter}>Counter 123</div>;
+}
